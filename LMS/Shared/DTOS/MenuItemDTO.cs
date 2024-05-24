394fd44e352388733ec441c3001ee1df734c7372
LeinerViloria <vilorialeiner2002@gmail.com>
2024-05-23 23:03:07 -0500

    Menu

---
+++ LMS/Shared/DTOS/MenuItemDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace LMS.Shared.DTOS
+{
+    public record MenuItemDTO
+    {
+        public string Title { get; set; } = null!;
+        public string Icon { get; set; } = null!;
+        public string? Url { get; set; }
+
+        public ICollection<MenuItemDTO> SubMenus { get; set; } = null!;
+    }
+}
